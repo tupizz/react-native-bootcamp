d35bb57f426717b2b1c5a4f5dd6b614f796d9792
tupizz <tadeu.tupiz@gmail.com>
2019-07-30 23:22:11 -0300

    main screen

--- src/index.js
+++ src/index.js
@@ -7,30 +7,18 @@
  */
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-    containter: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
+import Routes from './routes';
 
 const App = () => {
     return (
-        <View style={styles.containter}>
-            <Text style={styles.welcome}>Teste</Text>
-            <Text style={styles.welcome}>Abaixo</Text>
-        </View>
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <Routes />
+        </>
     );
 };
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
+
+const Main = () => {
+    return (
+        <Container>
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar usuário"
+                />
+                <SubmitButton>
+                    <Icon name="add" size={20} color="#FFF" />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
+};
+
+Main.navigationOptions = {
+    title: 'Usuários',
+};
+
+export default Main;
