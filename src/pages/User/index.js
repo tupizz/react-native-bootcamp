7e5cc3cc91a3b3b73324329c3e14481bebee90ed
tupizz <tadeu.tupiz@gmail.com>
2019-08-01 22:23:29 -0300

    Pagina de usuario

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,16 +1,87 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-console */
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-const User = ({ navigation }) => {
-    console.tron.log(navigation.getParam('user'));
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+} from './styles';
 
-    return (
-        <View>
-            <Text>Olá mundo</Text>
-        </View>
-    );
-};
+export default class User extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: navigation.getParam('user').name,
+    });
 
-export default User;
+    static propTypes = {
+        navigation: PropTypes.shape({
+            getParam: PropTypes.func,
+        }).isRequired,
+    };
+
+    state = {
+        stars: [],
+        loading: false,
+    };
+
+    async componentDidMount() {
+        const { navigation } = this.props;
+        const user = navigation.getParam('user');
+
+        this.setState({ loading: true });
+
+        const response = await api.get(`/users/${user.login}/starred`);
+        this.setState({
+            stars: response.data,
+            loading: false,
+        });
+    }
+
+    render() {
+        const { navigation } = this.props;
+        const { stars, loading } = this.state;
+
+        const user = navigation.getParam('user');
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{ uri: user.avatar }} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+
+                {loading ? (
+                    <ActivityIndicator color="#7159c1" />
+                ) : (
+                    <Stars
+                        data={stars}
+                        keyExtractor={star => String(star.id)}
+                        renderItem={({ item }) => (
+                            <Starred>
+                                <OwnerAvatar
+                                    source={{ uri: item.owner.avatar_url }}
+                                />
+                                <Info>
+                                    <Title>{item.name}</Title>
+                                    <Author>{item.owner.login}</Author>
+                                </Info>
+                            </Starred>
+                        )}
+                    />
+                )}
+            </Container>
+        );
+    }
+}
